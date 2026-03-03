c59af5686e31b506e9fd2ca143dd67019579fcf8
banupriya-by <bybanupriya@gmail.com>
2026-03-03 21:43:32 +0530

    Array created.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
-class Main{
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
 
-//      Integer array
-        int[] array = {1, 2, 3, 4, 5};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter array size : ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+//        array value taken from user
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter value : ");
+            array[i] = scanner.nextInt();
+        }
+
+//        Printing an array
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println("-------------");
+
+//        or printing array using for each loop
+        for (int Array : array) {
+            System.out.print(Array + " ");
+        }
     }
 }
